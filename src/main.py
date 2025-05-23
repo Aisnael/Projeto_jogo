428bc5f37e20b64632a07a9896d2bef643070013
Aisnael <aisnaelromao5@gmail.com>
2025-05-23 17:23:46 -0300

    [ADICIONAR] configurações iniciais da tela (tam, largura e cores)

--- src/main.py
+++ src/main.py
@@ -1,3 +1,25 @@
 # 1- Configurações iniciais
 import pygame
 import random
+
+# iniciar o pygame
+pygame.init() 
+
+# criar tela
+pygame.display.set_caption('Jogo Snake Python') # Titulo
+largura = 1000
+altura = 800
+pygame.display.set_mode((largura, altura))
+
+# cores 
+preta = (0, 0, 0)
+branca = (255, 255, 255)
+amarelo = (255, 255, 0)
+verde = (0, 255, 0)
+
+
+# 2- Criar um loop infinito
+
+
+
+# 3- Desenhar os objetos do jogo na tela
